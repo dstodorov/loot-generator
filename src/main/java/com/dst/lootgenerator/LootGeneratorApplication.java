9d0f8a927d1a596101f41f3d96df1fcd641f9a1a
Dimitar Todorov <d.todorov.vn@gmail.com>
2025-02-02 16:28:03 +0200

    Initial commit

---
+++ src/main/java/com/dst/lootgenerator/LootGeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.dst.lootgenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LootGeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LootGeneratorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dst/lootgenerator/LootGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dst.lootgenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LootGeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
