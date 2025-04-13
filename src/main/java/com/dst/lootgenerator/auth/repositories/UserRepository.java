c8f139da561bb4eac51428b9da18357e92b95b98
Dimitar Todorov <d.todorov.vn@gmail.com>
2025-04-13 23:10:18 +0300

    Fix params

--- src/main/java/com/dst/lootgenerator/auth/repositories/UserRepository.java
+++ src/main/java/com/dst/lootgenerator/auth/repositories/UserRepository.java
@@ -4,6 +4,7 @@ import com.dst.lootgenerator.auth.models.User;
 import com.dst.lootgenerator.player.models.dtos.PlayerHeroDto;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -11,14 +12,11 @@ import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-    Optional<User> findByEmail(String email);
+    Optional<User> findByEmail(@Param("email") String email);
 
     Optional<User> findByResetPasswordToken(String token);
 
-//    @Query("SELECT User.heroes h from User u WHERE u.id = :id ")
-//    List<Hero> findHeroesById(Long id);
-
     @Query("SELECT new com.dst.lootgenerator.player.models.dtos.PlayerHeroDto(h.id, h.name, h.level, h.heroClass) " +
             "FROM User u JOIN u.heroes h WHERE u.id = :id")
-    List<PlayerHeroDto> findPlayerHeroesBaseInfo(Long id);
+    List<PlayerHeroDto> findPlayerHeroesBaseInfo(@Param("id") Long id);
 }
